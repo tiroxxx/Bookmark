1ac2b2f08be97025d5a5bee040aa29cee0ca8882
tiroxxx <christianmarquez41@gmail.com>
2020-10-26 19:08:20 -0400

    added genre characteristic to the books model

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "bookmark",
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
